63326b5a48759d466458e4d87c0777d59dce1dd3
Chinmay <kchinmay294@gmail.com>
2025-07-16 23:45:11 +0530

    Subtraction program

---
+++ Sub.java
@@ -0,0 +1,12 @@
+class Sub{
+  int a = 20;
+  int b = 10;
+
+public static void main(String []args){
+      Sub subtraction = new Sub();
+      
+       int result = subtraction.a - subtraction.b;      
+
+      System.out.println("Subtraction of two numaber: " + result);
+  }
+}
